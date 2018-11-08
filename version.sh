26c3f2488b8ae251c36be743d765edf24cb7c036
i258559 <i258559@yeah.net>
2018-11-08 15:34:11 +0800

    Update version.sh

--- version.sh
+++ version.sh
@@ -19,7 +19,7 @@ export SSR_LINK="https://github.com/onekeyshell/shadowsocksr/archive/manyuser.zi
 export SSR_YUM_INIT="https://raw.githubusercontent.com/Aurwen/kcptun_for_ss_ssr/master/ssr.init"
 export SSR_APT_INIT="https://raw.githubusercontent.com/Aurwen/kcptun_for_ss_ssr/master/ssr_apt.init"
 # SSRR
-#export SSRR_VER=3.2.1
+#export SSRR_VER=3.2.2
 export SSRR_VER=$(wget --no-check-certificate -qO- https://raw.githubusercontent.com/onekeyshell/shadowsocksr/akkariiin/master/shadowsocks/version.py | grep return | cut -d\' -f2 | awk '{print $2}')
 export SSRR_LINK="https://github.com/onekeyshell/shadowsocksr/archive/akkariiin/master.zip"
 export SSRR_YUM_INIT="https://raw.githubusercontent.com/Aurwen/kcptun_for_ss_ssr/master/ssrr.init"
